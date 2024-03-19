3f0c60d13ddc7e02b688fe4c74402cdbb44028c9
Vishal Kumar <67638997+vishal8218@users.noreply.github.com>
2024-03-19 13:51:26 +0530

    Update imageTraverse.js

--- src/Componets/imageTraverse.js
+++ src/Componets/imageTraverse.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react'
 
 class HeadingDisplay extends Component{
-
     
     data=['https://i.redd.it/kriti-sanon-hot-v0-4aijraqzvndb1.jpg?width=1170&format=pjpg&auto=webp&s=d067d3dbe32dadf69348e28a9321def13ca17e0f','https://shorturl.at/crswE','https://shorturl.at/bnMP4','https://shorturl.at/cvCI3']
     
